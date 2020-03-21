e4a712fd836b57a0ce7a1758cff2dde78228902f
ANDREI AHUREIKIN <ahureikinandrei@gmail.com>
2020-03-21 18:08:38 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+  return Math.min.apply( Math, array );
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+  return Math.max.apply( Math, array );
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+  let sumArray = array.reduce( (sum, i) => sum + i );
+  let avgArray =  sumArray / array.length;
+  return avgArray;
+
 }
+
